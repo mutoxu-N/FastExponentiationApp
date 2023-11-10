ec6fcb5f572d43a9ba12036f7779481b6c8514f0
mutoxu=N <55544957+mutoxu-N@users.noreply.github.com>
2023-11-10 16:42:20 +0900

    Add UI&Logic

--- app/src/main/java/com/github/mutoxu_n/fastexponentiationapp/MainActivity.kt
+++ app/src/main/java/com/github/mutoxu_n/fastexponentiationapp/MainActivity.kt
@@ -2,10 +2,40 @@ package com.github.mutoxu_n.fastexponentiationapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.widget.doOnTextChanged
+import androidx.lifecycle.ViewModelProvider
+import com.github.mutoxu_n.fastexponentiationapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: MainActivityViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // init
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        viewModel = ViewModelProvider(this)[MainActivityViewModel::class.java]
+
+        binding.base.doOnTextChanged { text, _, _, _ ->
+            val v = Integer.parseInt(text.toString())
+            viewModel.setBase(v)
+        }
+
+        binding.exp.doOnTextChanged { text, _, _, _ ->
+            val v = Integer.parseInt(text.toString())
+            viewModel.setExp(v)
+        }
+
+        binding.num.doOnTextChanged { text, _, _, _ ->
+            val v = Integer.parseInt(text.toString())
+            viewModel.setNum(v)
+        }
+
+        viewModel.result.observe(this) { it?.let {
+            binding.result.text = it.toString()
+        } }
+
+        setContentView(binding.root)
     }
 }
